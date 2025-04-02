9174ac9bbe6b0eb82f11f0d583aa907d167620fe
Lucas Stasiak <108033145+StasiakLucas@users.noreply.github.com>
2025-04-02 22:39:41 +0000

    trabalhando com o botão de acessibilidade

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoAcessibilidade.addEventListener('click', function(){
+        botaoAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesAcessibilidade.classList.toggle('apresenta-lista');
+    });
+
     let tamanhoAtualFonte = 1;
 
     const botaoAumentarFonte = document.getElementById('aumentar-fonte');
